bcf8ffe694e061c332e2420ab975439a3a55eab6
José Ángel Martínez Socarrades <jangel.martinezs@gmail.com>
2022-06-28 23:31:04 +0200

    Creación cabecera con mensajes de error

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import NuevoPresupuesto from './NuevoPresupuesto'
+
+const Header = ({ presupuesto, setPresupuesto, isValidPresupuesto, setIsValidPresupuesto }) => {
+	return (
+		<header>
+			<h1>Panifiador de gastos</h1>
+
+			{
+				isValidPresupuesto
+					? (
+						<p>Control Presupuesto</p>
+					)
+					: (
+						<NuevoPresupuesto
+							presupuesto={presupuesto}
+							setPresupuesto={setPresupuesto}
+							setIsValidPresupuesto={setIsValidPresupuesto}
+						/>
+					)
+			}
+
+		</header>
+	)
+}
+
+export default Header
